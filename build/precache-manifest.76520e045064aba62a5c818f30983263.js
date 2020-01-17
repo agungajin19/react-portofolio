783bb877f189d31229f6e58832a48c465b20fc6a
agungajin19 <nugroho@alterra.id>
2020-01-17 20:02:19 +0700

    add pwa

---
+++ build/precache-manifest.76520e045064aba62a5c818f30983263.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d3019c9757aafaec6b317b68d7e579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "214c2d9947e35048ebaf",
+    "url": "/static/css/main.16a96638.chunk.css"
+  },
+  {
+    "revision": "5e57b17f39769ffb10ab",
+    "url": "/static/js/2.c1e1c76c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c1e1c76c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "214c2d9947e35048ebaf",
+    "url": "/static/js/main.c1b4b1ef.chunk.js"
+  },
+  {
+    "revision": "1b4d901d0c6ef5d4b5b0",
+    "url": "/static/js/runtime-main.061373c5.js"
+  },
+  {
+    "revision": "3644ee708bf9efe747ea78b2965ca58f",
+    "url": "/static/media/bg.3644ee70.jpg"
+  },
+  {
+    "revision": "f1b17b0b886cef7677c06e5b66381cc1",
+    "url": "/static/media/cart.f1b17b0b.png"
+  },
+  {
+    "revision": "91498f32c7d368d9a359d71defc3d60f",
+    "url": "/static/media/logo.91498f32.png"
+  },
+  {
+    "revision": "b48519351be92099287efcdcf19cfdb1",
+    "url": "/static/media/profile.b4851935.png"
+  },
+  {
+    "revision": "f9b0995a3fcca93d8c439eb93688a04d",
+    "url": "/static/media/zerocart.f9b0995a.png"
+  }
+]);
